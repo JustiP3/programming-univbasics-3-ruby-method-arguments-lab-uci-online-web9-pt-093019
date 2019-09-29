bc9161ef375e2533e8d21fb9945ecf76cc3ab6a1
Justin Phillips <justinphil02@yahoo.com>
2019-09-29 18:53:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,8 +11,8 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program #{language}."
 end 
 
-def introduction_with_language (name, language = "coding")
-  puts "Hi, my name is #{name} and I'm good at #{language}"
+def introduction_with_language (name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
 end 
 
 
